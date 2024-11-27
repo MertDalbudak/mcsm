a8c47b59c57a9928714a2b1f1a88270e3c98d4d0
MertDalbudak <mert.dalbudak@hotmail.com>
2024-11-27 01:20:07 +0100

    Check Entity Death

--- Server/Server.js
+++ Server/Server.js
@@ -336,6 +336,10 @@ class Server {
             // CHECK IF SOMEONE IS DEAD
             let check = null;
             let killed_by = null;
+            const check_entity_death = new RegExp(`^(?!.*[<>]).*Named entity .+\[.+\] died: `);
+            if(line.content.match(check_entity_death)){
+                line.content = line.content.replace(check_entity_death, '');
+            }
             for(let i = 0; i < death_messages.length; i++){
                 const check_kill = new RegExp(`([^<>]+) ${death_messages[i]}`);
                 check = line.content.match(check_kill);
@@ -348,6 +352,9 @@ class Server {
             }
             if(check != null){
                 let dying_player = check[0].split(" ")[0];
+                if(check[0].match(/Named entity (.)+\['(.)+'/)){
+                    console.log(check[0].match(/Named entity (.)+\['(.)+'/));
+                }
                 let death_message = "";
                 if(killed_by != null){
                     death_message = this.config.discord.killMessage.replace('{{killed_by}}', killed_by).replace('{{dying_player}}', dying_player);
